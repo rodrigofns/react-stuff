9a5cf85e1d488b3f66d48a5daf7389985f99baa4
Rodrigo Dias <rodrigo.dias@serpro.gov.br>
2017-09-04 08:25:19 -0300

    Added react-md material design lib.

--- src/form/Campos.jsx
+++ src/form/Campos.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TextField from 'react-md/lib/TextFields';
 
 export default class Campos extends Component {
 	constructor(props) {
@@ -6,8 +7,8 @@ export default class Campos extends Component {
 		this.state = { nome: '', local: '' };
 	}
 
-	mudou = (e) => {
-		this.setState({ [e.target.name]: e.target.value },
+	mudou = (val, e) => {
+		this.setState({ [e.target.id]: e.target.value },
 			() => {
 				this.props.mudouAlgo({
 					nome: this.state.nome,
@@ -18,18 +19,26 @@ export default class Campos extends Component {
 
 	render() {
 		return (
-			<table>
-				<tbody>
-					<tr>
-						<td>Nome:</td>
-						<td><input name="nome" size="20" autoFocus onChange={this.mudou}/></td>
-					</tr>
-					<tr>
-						<td>Local:</td>
-						<td><input name="local" size="20" onChange={this.mudou}/></td>
-					</tr>
-				</tbody>
-			</table>
+			<div>
+				{/* <table>
+					<tbody>
+						<tr>
+							<td>Nome:</td>
+							<td><input name="nome" size="20" autoFocus onChange={this.mudou}/></td>
+						</tr>
+						<tr>
+							<td>Local:</td>
+							<td><input name="local" size="20" onChange={this.mudou}/></td>
+						</tr>
+						<tr>
+							<td>Descrição:</td>
+							<td></td>
+						</tr>
+					</tbody>
+				</table> */}
+				<div><TextField label="Nome" id="nome" fullWidth={false} size={30} onChange={this.mudou}/></div>
+				<div><TextField label="Local" id="local" fullWidth={false} size={30} onChange={this.mudou}/></div>
+			</div>
 		);
 	}
 }
