1d4ac595c009b7a084640478cd919634942427c2
Rodrigo Dias <rodrigo.dias@serpro.gov.br>
2017-09-06 15:17:55 -0300

    Switch wrapper.

---
+++ src/material-design/Switch.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '@material/switch/dist/mdc.switch.min.css';
+import './fixes.css';
+
+export default class Switch extends Component {
+	static propTypes = {
+		defaultChecked: PropTypes.oneOf([true, false, 'defaultChecked']),
+		disabled: PropTypes.oneOf([true, false, 'disabled']),
+		name: PropTypes.string,
+		onChange: PropTypes.func
+	}
+
+	constructor(props) {
+		super(props);
+		this.state = { checked: false };
+	}
+
+	componentDidMount() {
+		if (this.props.defaultChecked) {
+			this.handleChange();
+		}
+	}
+
+	handleChange = (e) => {
+		this.setState({ checked: !this.state.checked }, () => {
+			if (this.props.onChange) {
+				this.props.onChange({
+					target: {
+						name: this.props.name,
+						value: this.state.checked
+					},
+					type: 'change'
+				});
+			}
+		});
+	}
+
+	render() {
+		let classes = 'mdc-switch' +
+			(this.props.disabled ? ' mdc-switch--disabled' : '');
+
+		return (
+			<label className="Switch-wrapLabel">
+				<div className={classes}>
+					<input type="checkbox"
+						name={this.props.name}
+						disabled={this.props.disabled}
+						defaultChecked={this.props.defaultChecked}
+						onChange={this.handleChange}
+						className="mdc-switch__native-control"/>
+					<div className="mdc-switch__background">
+						<div className="mdc-switch__knob"></div>
+					</div>
+				</div>
+				<div className="Switch-children">
+					{this.props.children}
+				</div>
+			</label>
+		);
+	}
+}
